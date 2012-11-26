661ee860b08973266f8bdda9ae47051b5e969ae6
Gytis <gytis.zilinskas@gmail.com>
2012-11-26 20:44:11 +0000

    improve speed (check the testing branch)

--- board.hs
+++ board.hs
@@ -6,13 +6,7 @@ type Board = Graph
 createSqBoard :: Int -> Board
 createSqBoard n = buildG (1, n*n) undirected
     where 
-        horizontal = foldl (\acc x -> if x `mod` n == 0 
-                                      then acc 
-                                      else acc ++ [(x, x+1)]
-                   ) [] [1..n*n]
-        vertical   = foldl (\acc x -> if x > n*(n-1) 
-                                      then acc 
-                                      else acc ++ [(x,x+n)]
-                   ) [] [1..n*n]
-        undirected = foldl (\acc (x,y) -> acc ++ [(y,x)]
+        horizontal = [(x, x+1) | x <- [1..n*n], x `mod` n /= 0]
+        vertical   = [(x, x+n) | x <- [1..n*n], x <= n*(n-1)]
+        undirected = foldl (\acc (x,y) -> ((y,x):acc)
                    ) (horizontal ++ vertical) (horizontal ++ vertical)
